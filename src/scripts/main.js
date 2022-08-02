ced738db46001e84e65fb8cd01bb47be212d10ed
ia <isaac.ardis@gmail.com>
2022-08-01 19:59:30 -0700

    add logic for cost/benefit calculation w/+w/o MESS
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
-// import { GetBacon } from './utils';
+import { ECBP1100_Penalty } from './utils';
 
-// const baconEl = document.querySelector('.bacon');
+const summaryTable = document.querySelector('#summary-table');
 
 // GetBacon()
 //   .then(res => {
@@ -8,3 +8,73 @@
 //     baconEl.innerHTML = markup;
 //   })
 //   .catch(err => (baconEl.innerHTML = err));
+
+const blockReward = 2.56;
+const blocksPerHour = 60.0 * 60 / 13.0;
+const usdETC = 36.0;
+
+// marketPriceHashrate defines the profit of a renter of hashrate in ETC/hour.
+//
+// We assume that the rental price for hashrate is higher than
+// the profit of the miners using the hardware for mining (rather than renting).
+//
+// If this were not true, and the price were LOWER,
+// the market should discover an arbitrage
+// and move rental hashrate to miner hashrate.
+//
+//
+// This value is defined as HOW MUCH MORE ETC/HOUR THE RENTER PAYS
+// compared to how much they could EARN if they were mining.
+const empiricalCost_Ethash_930THs_24h_ETH = 12910.1;
+const empiricalReward_Ethash_24h_ETH =  60 * 60 *24 / 13.5 * 2; // =12800
+const marketHashrateRentalCost =
+    empiricalCost_Ethash_930THs_24h_ETH /
+    empiricalReward_Ethash_24h_ETH; // eg. 1.0085
+
+function blockEmissionETC(hours) {
+    return blocksPerHour * blockReward * hours;
+}
+
+const attackDurationVals = [5, 10, 15, 30, 60, 90, 120,
+    60 *3, 60*4, 60*5, 60*6, 60*7, 60*8];
+
+for (let v of attackDurationVals) {
+    const row = document.createElement('tr');
+    const duration = document.createElement('td');
+    const blocks = document.createElement('td');
+    const cost = document.createElement('td');
+    const revenue = document.createElement('td');
+    const net = document.createElement('td');
+    const messPenalty = document.createElement('td');
+    const penalizedCost = document.createElement('td');
+    const penalizedNet = document.createElement('td');
+
+    duration.innerHTML = `${v} minutes`;
+    blocks.innerHTML = `${Math.round(v / 60 * blocksPerHour)}`;
+
+    const basisV = blockEmissionETC(v / 60) * usdETC;
+    const costV = (basisV * marketHashrateRentalCost);
+    cost.innerHTML = costV.toFixed(0);
+
+    const revenueV = basisV;
+    revenue.innerHTML = revenueV.toFixed(0);
+
+    net.innerHTML = (revenueV - costV).toFixed(0);
+
+    messPenalty.innerHTML = ECBP1100_Penalty(v * 60).toFixed(2);
+
+    penalizedCost.innerHTML = (costV * ECBP1100_Penalty(v * 60)).toFixed(0);
+
+    penalizedNet.innerHTML = (revenueV - penalizedCost.innerHTML).toFixed(0);
+
+    row.appendChild(duration);
+    row.appendChild(blocks);
+    row.appendChild(cost);
+    row.appendChild(revenue);
+    row.appendChild(net);
+    row.appendChild(messPenalty);
+    row.appendChild(penalizedCost);
+    row.appendChild(penalizedNet);
+
+    summaryTable.appendChild(row);
+}
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -5,3 +5,30 @@
 //
 //   return body;
 // };
+
+const ecbp1100PolynomialVCurveFunctionDenominator = 128;
+const ecbp1100PolynomialVXCap = 25132;
+const ecbp1100PolynomialVHeight = ecbp1100PolynomialVCurveFunctionDenominator * 15 * 2;
+
+export const ECBP1100_Polynomial = (x) => {
+    if (x > ecbp1100PolynomialVXCap) x = ecbp1100PolynomialVXCap;
+
+    let xa = 3 * (x ** 2);
+    let xb = (2 * (x ** 3)) / ecbp1100PolynomialVXCap;
+    let out = xa - xb;
+
+    out = out * ecbp1100PolynomialVHeight;
+
+    const xcap2 = ecbp1100PolynomialVXCap ** 2;
+    out = out / xcap2;
+
+    out = out + ecbp1100PolynomialVCurveFunctionDenominator;
+
+    return out;
+};
+
+export const ECBP1100_Penalty = (x) => {
+    return ECBP1100_Polynomial(x) / ecbp1100PolynomialVCurveFunctionDenominator;
+}
+
+console.log(ECBP1100_Penalty(60 * 60 * 2));
